d73364915437bf1acb50e94078678e0a0c421ec4
Fuad Saud <fuadfsaud@gmail.com>
2017-11-08 04:40:11 -0200

    Cleanup

--- src/nightbus/boundaries/coap/server.clj
+++ src/nightbus/boundaries/coap/server.clj
@@ -1,26 +1,21 @@
 (ns nightbus.boundaries.coap.server
   (:require [nightbus.kafka.producer :as kafka.producer]
             [nightbus.components :as components]
-            [taoensso.timbre :as log]
-            [clojure.data.json :as json])
+            [taoensso.timbre :as log])
   (:import (org.eclipse.californium.core CoapServer CoapResource)
            (org.eclipse.californium.core.coap CoAP$ResponseCode)
            (org.eclipse.californium.core.network CoapEndpoint)
-           (java.net InetAddress InetSocketAddress)
-           (nightbus.boundaries.coap.server.resources MessagesResource)))
+           (java.net InetAddress InetSocketAddress)))
 
-(defn messages-resource
+(defn make-messages-resource
   [{:keys [kafka-producer]}]
   (proxy [CoapResource] ["messages"]
     (handlePOST [exchange]
       (let [payload (nightbus.utils/tap (.getRequestPayload exchange))
             topic (nightbus.utils/tap (.getQueryParameter exchange "topic"))]
+        (log/info (str "[COAP SERVER]" {:post-message {:topic topic}}))
         (kafka.producer/produce! kafka-producer {:topic topic :payload payload})
-        (.respond exchange CoAP$ResponseCode/CHANGED)))
-
-    #_(getChild [resource-name]
-      (println resource-name)
-      (proxy-super getChild resource-name))))
+        (.respond exchange CoAP$ResponseCode/CHANGED)))))
 
 (defn start!
   [{coap-server-config :coap-server
@@ -29,8 +24,8 @@
         inet-address (InetAddress/getByName (:host coap-server-config))
         inet-socket-address (InetSocketAddress. inet-address (:port coap-server-config))
         coap-server (CoapServer.)
-        coap-endpoint (CoapEndpoint. inet-socket-address)]
-    ; (.add coap-server (into-array CoapResource [(MessagesResource. "messages")]))
-    (.add coap-server (into-array CoapResource [(messages-resource components)]))
+        coap-endpoint (CoapEndpoint. inet-socket-address)
+        messages-resource (make-messages-resource components)]
+    (.add coap-server (into-array CoapResource [messages-resource]))
     (.addEndpoint coap-server coap-endpoint)
     (.start coap-server)))
--- src/nightbus/boundaries/coap/server/resources/MessagesResource.clj
+++
@@ -1,3 +0,0 @@
-(ns nightbus.boundaries.coap.server.resources.MessagesResource
-  (:gen-class
-    :extends org.eclipse.californium.core.CoapResource))
