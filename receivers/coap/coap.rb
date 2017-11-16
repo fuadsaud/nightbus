ee6416265c248eee5a38678f44bc8a498fd8cce4
Fuad Saud <fuadfsaud@gmail.com>
2017-11-16 14:34:51 -0200

    benchmark stuff

--- receivers/coap/coap.rb
+++ receivers/coap/coap.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/env ruby
-#
+
+require 'date'
+
 module Nightbus
   class CoAP
+    LOG_FILENAME = '/Users/fuad/Code/fuadsaud/nightbus/benchmark/http-coap/t3'.freeze
+
     def call(env)
       puts env
+
+      IO.write(LOG_FILENAME, "#{DateTime.now.strftime('%Q')}\n", mode: 'a')
 
       [200, {}, ['ok']]
     end
