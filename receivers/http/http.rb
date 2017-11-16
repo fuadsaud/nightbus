da30ab9005d2deb68c3b05d29d21fcd0d75087a9
Fuad Saud <fuadfsaud@gmail.com>
2017-11-16 14:35:15 -0200

    more receivers

--- receivers/http/http.rb
+++ receivers/http/http.rb
@@ -2,8 +2,12 @@
 
 module Nightbus
   class HTTP
+    LOG_FILENAME = '/Users/fuad/Code/fuadsaud/nightbus/benchmark/coap-http/t3'.freeze
+
     def call(env)
       puts env
+
+      IO.write(LOG_FILENAME, "#{DateTime.now.strftime('%Q')}\n", mode: 'a')
 
       [200, {}, ['ok']]
     end
